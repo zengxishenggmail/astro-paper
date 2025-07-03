22336ba2a1879d3e1a998e1ddc8d6fa3e95dd6a1
zengxishenggmail <136914475+zengxishenggmail@users.noreply.github.com>
2025-07-03 11:36:01 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Sat Naing",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "BIC Post",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
